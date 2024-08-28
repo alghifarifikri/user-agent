c3a2c6f8a8b2bd7ae49dfd79715cc798a907af7a
alghifarifikri <alghi77@gmail.com>
2024-08-28 18:53:49 +0700

    fix button & tailwind

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,28 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 interface ButtonProps {
   label: string;
-  className: string;
+  className?: string;
   disabled: boolean;
   onClick: () => void;
 }
 
-export default function Button({
+export default function ButtonCustom({
   label,
   className,
   disabled = false,
   onClick,
 }: ButtonProps) {
   return (
-    <button className={className} onClick={onClick} disabled={disabled}>
+    <Button
+      variant="contained"
+      className={className}
+      style={{ marginLeft: "2px", marginRight: "2px" }}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {label}
-    </button>
+    </Button>
   );
 }
